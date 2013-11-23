3d1c7fee6e9140739020a02535165b73e34cae47
Shindo N. Strzelczyk <shindo@Shindos-MacBook-Pro.local>
2013-11-23 12:27:01 -0800

    Added Bootstrap and feature to add images from text

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
-console.log('testing')
-console.log(jQuery)
+$(function() {
+  console.log('testing');
+  console.log(jQuery);
+
+  $("button").click(function(e) {
+    e.preventDefault();
+    console.log(bgColor);
+    var size = $("#size").val(),
+    bgColor = $("#bgColor").val(),
+    fgColor = $("#fgColor").val(),
+    originalText = $("#text").val(),
+    finalText = originalText.split(' ').join('+');
+    console.log(finalText);
+    $("#image_display").html('<img src="http://dummyimage.com/' + size + '/' + bgColor + '/' + fgColor + '.png&text=' + finalText + '">');
+  });
+});

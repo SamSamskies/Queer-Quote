579b167bb8576dcb7e0a6558360fbf2fab465c1b
Steven Miyakawa SAM <samprofessional@gmail.com>
2013-11-23 15:37:32 -0800

    Refactor JS code a little

--- js/app.js
+++ js/app.js
@@ -1,15 +1,31 @@
 $(document).ready(function() {
   Player.init( '#soundcloud', 'http://soundcloud.com/outloud-radio-1/a-trans-cendent-perspective' );
-
-  $("button").click(function(e) {
-    e.preventDefault();
-    console.log(bgColor);
-    var size = $("#size").val(),
-    bgColor = $("#bgColor").val(),
-    fgColor = $("#fgColor").val(),
-    originalText = $("#text").val(),
-    finalText = originalText.split(' ').join('+');
-    console.log(finalText);
-    $("#image_display").html('<img src="http://dummyimage.com/' + size + '/' + bgColor + '/' + fgColor + '.png&text=' + finalText + '">');
-  });
+  App.initListeners();
 });
+
+
+var App = {
+  initListeners: function() {
+    $("button").click(function(e) {
+      e.preventDefault();
+      console.log(bgColor);
+      var size = $("#size").val(),
+      bgColor = $("#bgColor").val(),
+      fgColor = $("#fgColor").val(),
+      originalText = $("#text").val(),
+      finalText = originalText.split(' ').join('+');
+      console.log(finalText);
+      $("#image_display").html('<img src="http://dummyimage.com/' + size + '/' + bgColor + '/' + fgColor + '.png&text=' + finalText + '">');
+    });
+  }
+}
+
+// <div id="mImageBox">
+//     <img id='my_image' src='http://www.catster.com/files/original.jpg'/>
+// </div>
+// <script>
+//     $('#my_image').click(function(e) {
+//         u = this.src;
+//         window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent(u));
+//     })
+// </script>

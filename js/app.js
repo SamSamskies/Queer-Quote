c639a8fbf3901f686d0f00ba1746fab70f112b9e
Steven Miyakawa SAM <samprofessional@gmail.com>
2013-11-23 23:43:13 -0800

    More JS refactoring

--- js/app.js
+++ js/app.js
@@ -1,20 +1,23 @@
 $(document).ready(function() {
   Player.init({
-    container: '#soundcloud',
-    footnoteTarget: 'footnote',
+    container: App.soundcloudContainerId,
+    footnoteTarget: App.footnoteTarget,
     soundcloudUrl: App.soundcloudUrl,
     srtUrl: App.srtApiEndpoint + App.srtUrl
   });
   App.initListeners();
 });
 
-var App = {
 
-  srtApiEndpoint: 'http://srt2json.herokuapp.com/?url=',
+var App = {
 
   // Update these 2 urls to change what podcast gets loaded on the page
   soundcloudUrl: 'http://soundcloud.com/outloud-radio-1/a-trans-cendent-perspective',
   srtUrl: 'http://new.outloudradio.org/sites/default/files/transcripts/A_Trans-cendent_Perspective.en_.srt',
+
+  soundcloudContainerId: '#soundcloud',
+  footnoteTarget: 'footnote',
+  srtApiEndpoint: 'http://srt2json.herokuapp.com/?url=',
 
   initListeners: function() {
 

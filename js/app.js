3f9ab910d7386b00193f7b10c2efbbeabeda365e
nathanallen <thenathanator@gmail.com>
2013-11-26 18:07:55 -0800

    move location hash controller to its own file

--- js/app.js
+++ js/app.js
@@ -66,15 +66,3 @@ var App = {
 
   }
 };
-
-var LocationHash = {
-  parse: function(){
-    var hashstr = window.location.hash.substring(1)
-    if (hashstr.length) 
-      LocationHash.permalink = hashstr
-  },
-  permalink: App.soundcloudPermalink,
-  update: function(newPermalink){
-    window.location.hash = newPermalink
-  }
-}

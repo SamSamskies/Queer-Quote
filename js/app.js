698d9a0766951b4eb50bbb9057a19ac4e3440afa
Shindo Strzelczyk <shindostrz@gmail.com>
2014-05-03 14:36:37 -0700

    Removed unnecessary variables and commented code

--- js/app.js
+++ js/app.js
@@ -49,14 +49,11 @@ var App = {
     });
 
     $('button#share_fb').on('click', function share() {
-      var share_canvas =
-        Canvas;
-        // new Canvas(document.getElementById("share_canvas"), can.getContext("2d"), 1100, 70, (this.can.width - this.maxWidth / 2), (this.can.height / 2), 64, "bold " + this.default_font +"pt Berkshire Swash");
       var img;
       try {
-        img = share_canvas.can.toDataURL().split(',')[1];
+        img = Canvas.can.toDataURL().split(',')[1];
       } catch(e) {
-        img = share_canvas.can.toDataURL('image/jpeg').split(',')[1];
+        img = Canvas.can.toDataURL('image/jpeg').split(',')[1];
       }
       var w = window.open();
       w.document.write('Uploading... This may take a moment.');
@@ -74,7 +71,6 @@ var App = {
         },
         dataType: 'json'
       }).success(function(data) {
-          // w.close();
           var imgurLink = data.data.link;
           w.location = imgurLink.replace(".png", "");
 
